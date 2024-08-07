b48bfd5c2c7a3e17fce369d8ac289683625b7f29
SergeantSage <empenogodosage@gmail.com>
2024-08-07 18:29:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["969074776448569344", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
